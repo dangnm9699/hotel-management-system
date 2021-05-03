0176c4cc18d6270f968fe42ec5a733f263fa5617
VikingTC <nguyenkytungcntt04@gmail.com>
2021-05-03 17:56:14 +0700

    update config backend

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,12 @@
+require("dotenv").config();
+exports.port = process.env.PORT || 3001
 exports.accessTokenLife = process.env.ACCESS_TOKEN_LIFE || 30000;
 exports.accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || "ACCESS_TOKEN_SECRET";
 exports.refreshTokenLife = process.env.REFRESH_TOKEN_LIFE || "30d";
 exports.refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET || "REFRESH_TOKEN_SECRET";
-exports.refreshTokenCookieLife= 30*24*60*60*1000;
+exports.refreshTokenCookieLife = process.env.COOKIE_LIFE || 30 * 24 * 60 * 60 * 1000;
+exports.dbHost = process.env.DB_HOST || "172.0.0.1"
+exports.db = process.env.DB || "CD"
+exports.userDB = process.env.USER_DB || "root";
+exports.passwordDB = process.env.PASSWORD_DB || "";
+exports.saltRounds = Number(process.env.SALT_ROUNDS) || 10
