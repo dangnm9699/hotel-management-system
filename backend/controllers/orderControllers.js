08c16822bf31f079cb7d0ac57bab8b63a087d7cb
Đức Nguyễn <ntd275@gmail.com>
2021-05-11 23:38:50 +0700

    add order controller

---
+++ backend/controllers/orderControllers.js
@@ -0,0 +1,123 @@
+const Order = require('../models/Order')
+
+exports.createOrder = async function(req, res){
+    try {
+        let id = await Order.createOrder(req.body)
+        res.json({
+            success: true,
+            id
+        })
+    } catch (err) {
+        console.log(err)
+        if(typeof err === 'string' && err.startsWith('Phòng ')){
+            res.status(400).json({
+                success: false,
+                err,
+            })
+        }else{
+            res.status(500).json({
+                success: false,
+                err,
+            })
+        }
+    }
+}
+exports.getOrder = async function(req, res){
+    try {
+        let data = await Order.createOrder(req.body)
+        res.json({
+            success: true,
+            data: data,
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false,
+            err,
+        })
+    }
+}
+exports.getListOrderToCheckInByDay = async function (req, res) {
+    try {
+        let page = parseInt(req.query.page, 10) || 1
+        let perpage = parseInt(req.query.perpage) || 1000
+        let result = await Order.getListOrderToCheckInByDay(page, perpage)
+        result.success = true
+        res.json(result)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false,
+            err,
+        })
+    }
+}
+exports.getListOrderToCheckInByTomorrow = async function (req, res) {
+    try {
+        let page = parseInt(req.query.page, 10) || 1
+        let perpage = parseInt(req.query.perpage) || 1000
+        let result = await Order.getListOrderToCheckInByTomorrow(page, perpage)
+        result.success = true
+        res.json(result)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false,
+            err,
+        })
+    }
+}
+exports.getListOrderToCheckInByWeek = async function (req, res) {
+    try {
+        let page = parseInt(req.query.page, 10) || 1
+        let perpage = parseInt(req.query.perpage) || 1000
+        let result = await Order.getListOrderToCheckInByWeek(page, perpage)
+        result.success = true
+        res.json(result)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false,
+            err,
+        })
+    }
+}
+
+exports.getOrder = async function (req, res) {
+    try {
+        let order = await Order.getOrder(req.params.id)
+        if (!order) {
+            res.status(404).json({
+                success: false,
+                message: "Room not found"
+            })
+            return
+        }
+        res.json({
+            success: true,
+            data: order,
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false,
+            err,
+        })
+    }
+}
+
+exports.checkIn = async function (req, res) {
+    try {
+        await Order.getOrder(req.params.id)
+        res.json({
+            success: true,
+            data: req.params.id,
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false,
+            err,
+        })
+    }
+}
