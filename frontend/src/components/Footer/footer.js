af2b816b912c781f75a22a1da4997a6d8fa08c6e
NMHIEU2021999 <minhhieu20219992@gmail.com>
2021-05-24 23:51:25 +0700

    responsive footer

--- frontend/src/components/Footer/footer.js
+++ frontend/src/components/Footer/footer.js
@@ -7,14 +7,14 @@ export default class Footer extends React.Component {
             <footer className="footer">
                 <div className="container bottom_border">
                     <div className="row">
-                        <div className=" col-sm-4 col-md col-sm-4  col-12 col">
+                        <div className="col-3">
                             <h5 className="headin5_amrc col_white_amrc pt2">Find us</h5>
                             <p className="mb10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
                             <p><i className="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35 </p>
                             <p><i className="fa fa-phone"></i>  +91-9999878398  </p>
                             <p><i className="fa fa fa-envelope"></i> info@example.com  </p>
                         </div>
-                        <div className=" col-sm-4 col-md  col-6 col">
+                        <div className="col-3">
                             <h5 className="headin5_amrc col_white_amrc pt2">Quick links</h5>
                             <ul className="footer_ul_amrc">
                                 <li><a href="#">Image Rectoucing</a></li>
@@ -25,7 +25,7 @@ export default class Footer extends React.Component {
                                 <li><a href="#">Image Cropping</a></li>
                             </ul>
                         </div>
-                        <div className=" col-sm-4 col-md  col-6 col">
+                        <div className="col-3">
                             <h5 className="headin5_amrc col_white_amrc pt2">Quick links</h5>
                             <ul className="footer_ul_amrc">
                                 <li><a href="#">Remove Background</a></li>
@@ -36,7 +36,7 @@ export default class Footer extends React.Component {
                                 <li><a href="#">Image Cropping</a></li>
                             </ul>
                         </div>
-                        <div className=" col-sm-4 col-md  col-12 col">
+                        <div className=" col-3">
                             <h5 className="headin5_amrc col_white_amrc pt2">Follow us</h5>
                             <ul className="footer_ul2_amrc">
                                 <li><a href="https://www.facebook.com/nguyentheduc99" target="_blank"><i className="fab fa-facebook fleft padding-right"></i> <p>Nguyễn Thế Đức</p></a></li>
